026d75783495520e2a2fc2930970f151d832f03d
Luis Najera <najeraluis600@gmail.com>
2021-04-02 23:05:10 -0500

    Maps API and Heatmap
    added maps API key
    
    created basic layout for heatmap

--- hotspots/lib/screens/Home/DiscoverPage.dart
+++ hotspots/lib/screens/Home/DiscoverPage.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter_heatmap/google_maps_flutter_heatmap.dart';
 
 class DiscoverPage extends StatefulWidget{
 
@@ -12,88 +15,63 @@ class DiscoverPage extends StatefulWidget{
 }
 
 class _DiscoverPage extends State<DiscoverPage>{
+  Completer<GoogleMapController> _controller = Completer();
+  final Set<Heatmap> _heatmaps = {};
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  LatLng _heatmapLocation = LatLng(37.42796133580664, -122.085749655962);
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
-    var list = [{'name':"Adam Aneve","comment":"First!"},
-                {'name':"Bob Alice","comment":"I wonder if anyone's listening."},
-                {'name':"John Smith","comment":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-                {'name':"Richard Bag","comment":"^ loser lol"},
-                {'name':"John Smith","comment":"Pardon me sir, but what is it that you just said about me? I'll have you know that I graduated with honors"}];
-    return Material(
-        color: Colors.white,
-          child: Column(
-            children: [
-              SizedBox(height: 5.0),
-              Row(children: [
-                //TODO WHEN POSTS ARE IMPLEMENTED: ROUTE.
-                IconButton(icon: Icon(Icons.arrow_back), onPressed: (){ print("Return to Post"); },)
-              ]),
-              Divider(color: Colors.black54, thickness: 1.0),
-              Expanded(
-                  child: ListView(
-                    shrinkWrap: true,
-                    physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-                    children: [
-                      for(var item in list )
-                        CommentBox(content: item)
-                    ],
-                  )
-              )
-            ],
-          )
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        heatmaps: _heatmaps,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _addHeatmap,
+        label: Text('Add Heatmap'),
+        icon: Icon(Icons.add_box),
+      ),
     );
   }
-}
-class CommentBox extends StatefulWidget{
-  final Map<String,String> content;
-  CommentBox({Key key, this.content}) : super(key: key);
-  @override
-  _CommentBox createState() => _CommentBox();
-}
-
-class _CommentBox extends State<CommentBox>{
-  @override
-  Widget build(BuildContext context){
-    return RaisedButton(color: Colors.white,onPressed: (){}, elevation: 0.0, child: Container(
-        child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 5.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                      height: 50.0,
-                      width: 50.0,
-                      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.black12)
-                  ),
-                ),
-                Padding(padding: EdgeInsets.symmetric(horizontal: 2.0, vertical: 0.0)),
-                Expanded(
-                    child: Container(
-                        margin: EdgeInsets.fromLTRB(5.0,5.0,0.0,0.0),
-                        alignment: Alignment.bottomLeft,
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              children: [
-                                Text(widget.content['name'], maxLines: 1, overflow: TextOverflow.ellipsis, style: TextStyle(color: Colors.black54, fontWeight: FontWeight.bold))
-                              ],
-                            ),
-                            Row(
-                                children: [
-                                  Flexible(
-                                      child: Text(widget.content['comment'], overflow: TextOverflow.visible, style: TextStyle(color: Colors.black45))
-                                  )
-                                ]
-                            )
-                          ],
-                        )
-                    )
-                )
-              ],
-            )
-        )
-    ));
+  void _addHeatmap(){
+    setState(() {
+      _heatmaps.add(
+          Heatmap(
+              heatmapId: HeatmapId(_heatmapLocation.toString()),
+              points: _createPoints(_heatmapLocation),
+              radius: 20,
+              visible: true,
+              gradient:  HeatmapGradient(
+                  colors: <Color>[Colors.green, Colors.red], startPoints: <double>[0.2, 0.8]
+              )
+          )
+      );
+    });
   }
+  //heatmap generation helper functions
+  List<WeightedLatLng> _createPoints(LatLng location) {
+    final List<WeightedLatLng> points = <WeightedLatLng>[];
+    //Can create multiple points here
+    points.add(_createWeightedLatLng(location.latitude,location.longitude, 1));
+    points.add(_createWeightedLatLng(location.latitude-1,location.longitude, 1));
+    return points;
+  }
+
+  WeightedLatLng _createWeightedLatLng(double lat, double lng, int weight) {
+    return WeightedLatLng(point: LatLng(lat, lng), intensity: weight);
+  }
+
 }
