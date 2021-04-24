cd27a8c7c7caaabc939ce8abdbbb1b066aa3152c
Luis Najera <najeraluis600@gmail.com>
2021-04-24 16:54:28 -0500

    Get heapmap points from database
    points plotted in heatmap now come from the database rather than hardcoded points

--- hotspots/lib/screens/Home/DiscoverPage.dart
+++ hotspots/lib/screens/Home/DiscoverPage.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_heatmap/google_maps_flutter_heatmap.dart';
@@ -46,66 +47,35 @@ class _DiscoverPage extends State<DiscoverPage>{
       ),
     );
   }
-  void _addHeatmap(){
+
+  void _addHeatmap() async{
     LatLng _heatmapLocation = LatLng(32.733114, -97.112524);
     List<WeightedLatLng> heatmapPoints = _createPoints(_heatmapLocation);
 
     //TODO: for each post in the past day whose coordinates are in the visible area, add a heatmap point with said coordinates
+    Widget serverHeatmapPoints = FutureBuilder(
+        future: FirebaseFirestore.instance.collection("Post")
+        //.where("timestamp", isGreaterThanOrEqualTo: "TODO: yesterday")
+        //.where("latitude", isGreaterThan: "//TODO: map coordinate barrier").where("latitude", isLessThan: "TODO: map coordinate barrier")
+        //.where("longitude", isGreaterThan: "//TODO: map coordinate barrier").where("longitude", isLessThan: "TODO: map coordinate barrier"))
 
-    _heatmapLocation = LatLng(32.73060330871282, -97.1129670622124);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
+            .limit(100)
+            .get(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+          if(snapshot.hasError || !snapshot.hasData) return new Center(child: CircularProgressIndicator());
+          else{
+            for(var i = 0; i < snapshot.data.docs.length; i++){
+              Map<String, dynamic> info = snapshot.data.docs.elementAt(i).data();
 
-    _heatmapLocation = LatLng(32.729, -97.1130);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
+              _heatmapLocation = LatLng(info["latitude"], info["longitude"]);
+              heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
+            }
+          }
+          // this part does not get used but is required by the function
+          return Text('Hello, world!', textDirection: TextDirection.ltr,);
+        }
+    );
 
-    _heatmapLocation = LatLng(32.7285, -97.1135);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.7298, -97.1129);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.731114, -97.112724);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.732114, -97.112824);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.733514, -97.112554);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.733314, -97.112624);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.733214, -97.112574);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.73414, -97.112654);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-    _heatmapLocation = LatLng(32.733314, -97.112624);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.733214, -97.112574);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.73514, -97.112754);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.73414, -97.112454);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.73414, -97.112654);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-    _heatmapLocation = LatLng(32.733344, -97.112724);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.733234, -97.112374);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.73344, -97.112654);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
-
-    _heatmapLocation = LatLng(32.735314, -97.112554);
-    heatmapPoints.insertAll(0, _createPoints(_heatmapLocation));
     setState(() {
       _heatmaps.add(
           Heatmap(
